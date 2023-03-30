499af8ee14f7076b50f79cd70c1f10fe130d12e2
Cristhian Martinez <cristhiannotifications@gmail.com>
2023-03-30 17:38:13 -0300

    feat(): Agrego avances de la pantalla de búsqueda.

--- lib/search/search_department.dart
+++ lib/search/search_department.dart
@@ -1,96 +1,3 @@
-/* import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-
-import '../utils/utils.dart';
-import '../widgets/widget.dart';
-
-class SearchDeparment extends StatelessWidget {
-  const SearchDeparment({Key ?key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    bool _showLocation = false;
-    return Scaffold(
-      body: SafeArea(
-        child: Container(
-          width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.height,
-          color: Colors.white,
-          child: ListView(
-            physics: const BouncingScrollPhysics(),
-            shrinkWrap: true,
-            children: [
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const BackButtonGeneralWidget(),
-                  const SizedBox(height: Dimensions.heightSize),
-                  const _SearchBar(),
-                  const SizedBox(height: Dimensions.heightSize),
-                  Column(
-                    children: [
-                      Container(
-                        color: Colors.grey[200],
-                        padding: const EdgeInsets.symmetric(vertical: 8),
-                        child: const _LocationListItem(location: 'Av Holanda 754'),
-                      )
-                    ],
-                  ),
-                ],
-              ),
-            ],
-          )
-        )
-        ),
-    );
-  }
-}
-
-class _SearchBar extends StatelessWidget {
-  const _SearchBar({
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-    padding: const EdgeInsets.symmetric(horizontal: 30),
-    child: TextField(
-      decoration:  InputDecoration(
-        contentPadding: const EdgeInsets.symmetric(
-          vertical: 8,  // altura de 32 - 16 (padding vertical)
-          horizontal: 14, // anchura de 285 - 16 (padding horizontal)
-        ),
-        border: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(20),
-          borderSide: const BorderSide(
-            width: 1,
-            style: BorderStyle.solid,
-            color: CustomColor.greyColor
-          )
-        ),
-        suffixIcon: const Icon(FontAwesomeIcons.magnifyingGlass, color: CustomColor.greyColor, size: 16,),
-        hintText: 'Búsquedas',
-      ),
-    ),
-  );
-  }
-}
-
-class _LocationListItem extends StatelessWidget {
-  const _LocationListItem({Key? key, required this.location})
-      : super(key: key);
-
-  final String location;
-
-  @override
-  Widget build(BuildContext context) {
-    return ListTile(
-      title: Text(location),
-    );
-  }
-} */
-
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
@@ -120,7 +27,7 @@ class _SearchDepartmentState extends State<SearchDepartment> {
             shrinkWrap: true,
             children: [
               Column(
-                mainAxisAlignment: MainAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.start,
                 children: [
                   const BackButtonGeneralWidget(),
                   const SizedBox(height: Dimensions.heightSize),
@@ -133,11 +40,8 @@ class _SearchDepartmentState extends State<SearchDepartment> {
                       });
                     },
                     child: Container(
-                      padding: const EdgeInsets.symmetric(vertical: 8),
-                      width: MediaQuery.of(context).size.width * 0.5,
-                      color: CustomColor.redColor,
+                      padding: EdgeInsets.symmetric(vertical: 8, horizontal: MediaQuery.of(context).size.width * .2),
                       child:  Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
                         children: [
                           const Icon(
                             FontAwesomeIcons.podcast,
@@ -169,21 +73,56 @@ class _SearchDepartmentState extends State<SearchDepartment> {
                     ),
                   ),
                   SizedBox(height: MediaQuery.of(context).size.height - (MediaQuery.of(context).size.height * .97),),
-                  if (_showLocation)
-                    ClipRRect(
-                      borderRadius: BorderRadius.circular(10),
-                      child: Container(
-                        color: Colors.grey[200],
-                        width: MediaQuery.of(context).size.width * 0.7,
-                        child:  Column(
-                          children: [
-                            const _LocationListItem(comuna: 'Av. Providencia. Santiago', direction: 'Av Holanda 754 ',),
-                            const _LocationListItem(comuna: 'Av. Providencia. Santiago', direction: 'Av Holanda 754',),
-                            const _LocationListItem(comuna: 'Av. Providencia. Santiago', direction: 'Av Holanda 754',),
-                          ],
-                        ),
+                    Container( 
+                      child: _showLocation ?
+                        ClipRRect(
+                          borderRadius: BorderRadius.circular(10),
+                          child: Container(
+                            color: Colors.grey[200],
+                            width: MediaQuery.of(context).size.width * 0.73,
+                            child:  Column(
+                              children:  const[
+                                _LocationListItem(comuna: 'Av. Providencia. Santiago', direction: 'Av Holanda 754 ', icon: FontAwesomeIcons.podcast, colorDirection: CustomColor.brownColor2,),
+                                _LocationListItem(comuna: 'Av. Providencia. Santiago', direction: 'Av Holanda 754', icon: FontAwesomeIcons.podcast, colorDirection: CustomColor.brownColor2),
+                                _LocationListItem(comuna: 'Av. Providencia. Santiago', direction: 'Av Holanda 754',icon: FontAwesomeIcons.podcast, colorDirection: CustomColor.brownColor2),
+                              ],
+                            ),
+                          ),
+                        ) : Container(
+                          padding: EdgeInsets.symmetric(horizontal: MediaQuery.of(context).size.width * .1),
+                          child: Text( 
+                            'Búsquedas recientes',
+                            style: TextStyle(
+                              fontSize: Dimensions.semilarge,
+                              fontWeight: FontWeight.w600
+                            ),
+                            ),
+                        )
                       ),
+                  SizedBox(height: MediaQuery.of(context).size.height - (MediaQuery.of(context).size.height * .98),),
+                  Container(
+                    width: MediaQuery.of(context).size.width * 0.73,
+                    child: Column(
+                      children: const[
+                        _LocationListItem(
+                          comuna: 'Av. Providencia. Santiago', 
+                          direction: 'Santiago', 
+                          icon: FontAwesomeIcons.clockRotateLeft, 
+                          colorDirection: CustomColor.colorBlack),
+                      ],
                     ),
+                  ),
+                  SizedBox(height: MediaQuery.of(context).size.height - (MediaQuery.of(context).size.height * .98),),
+                  Container(
+                          padding: EdgeInsets.symmetric(horizontal: MediaQuery.of(context).size.width * .1),
+                          child: Text( 
+                            'Ver todas las búsquedas recientes',
+                            style: TextStyle(
+                              fontSize: Dimensions.defaultTextSize + 1,
+                              fontWeight: FontWeight.w600
+                            ),
+                            ),
+                        )
                 ],
               ),
             ],
@@ -230,10 +169,12 @@ class _SearchBar extends StatelessWidget {
 }
 
 class _LocationListItem extends StatelessWidget {
-  const _LocationListItem({Key? key, required this.comuna, required this.direction}) : super(key: key);
+  const _LocationListItem({Key? key, required this.comuna, required this.direction, required this.icon, required this.colorDirection}) : super(key: key);
 
   final String comuna;
   final String direction;
+  final IconData icon;
+  final Color colorDirection;
 
   @override
   Widget build(BuildContext context) {
@@ -243,40 +184,38 @@ class _LocationListItem extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
 
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: MediaQuery.of(context).size.width * 0.1, vertical: 2),
-            child: Row(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: [
-                          const Icon(
-                            FontAwesomeIcons.podcast,
-                            color: CustomColor.greenColor,
-                            ),
-                            SizedBox( width: MediaQuery.of(context).size.width * .03,),
-                          RichText(
-                            text: TextSpan(
-                              text: '$direction\n',
-                              style: TextStyle(
-                                fontWeight: FontWeight.w600,
-                                fontSize: Dimensions.defaultTextSize + 1,
-                                color: CustomColor.brownColor2
-                              ),
-                              children: [
-                                TextSpan(
-                                  text: comuna,
-                              style: TextStyle(
-                                fontWeight: FontWeight.normal,
-                                fontSize: Dimensions.smallTextSize,
-                                color: CustomColor.colorBlack
-                              ),
-                                )
-                              ]
-                              
-                              ),
+          Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        SizedBox( width: MediaQuery.of(context).size.width * .07,),
+                        Icon(
+                          icon,
+                          color: CustomColor.greenColor,
                           ),
-                        ],
-                      ),
-          ),
+                          SizedBox( width: MediaQuery.of(context).size.width * .03,),
+                        RichText(
+                          text: TextSpan(
+                            text: '$direction\n',
+                            style: TextStyle(
+                              fontWeight: FontWeight.w600,
+                              fontSize: Dimensions.defaultTextSize + 1,
+                              color: colorDirection
+                            ),
+                            children: [
+                              TextSpan(
+                                text: comuna,
+                            style: TextStyle(
+                              fontWeight: FontWeight.normal,
+                              fontSize: Dimensions.smallTextSize,
+                              color: CustomColor.colorBlack
+                            ),
+                              )
+                            ]
+                            
+                            ),
+                        ),
+                      ],
+                    ),
         ],
       ),
     );
